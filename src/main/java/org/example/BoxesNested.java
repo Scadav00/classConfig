4e1c6695becb5e4c899ccfd99d59870bc1a5b5b4
FilipOruzinsky <oruzinskyfilip@gmail.com>
2022-10-19 15:01:07 +0200

    nestedJson_serialization

---
+++ src/main/java/org/example/BoxesNested.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class BoxesNested {
+    private String name1;
+    private String name2;
+    private String name3;
+    private String name4;
+
+    public BoxesNested(String name1, String name2, String name3, String name4) {
+        this.name1 = name1;
+        this.name2 = name2;
+        this.name3 = name3;
+        this.name4 = name4;
+    }
+}
